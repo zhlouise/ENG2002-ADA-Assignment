5eb1009ef49eacfee93ba23e25231145f0b9e492
zhlouise <116058486+zhlouise@users.noreply.github.com>
2023-11-26 03:25:41 +0000

    change in main

--- main.py
+++ main.py
@@ -2,12 +2,16 @@
 def main_menu():
   # Loop to display main menu, where the user is repeatly asked to choose an operation
   while True: 
-    print ("I: Input data")
+    # Print possible operation choices
+    print ("D: Perform rational number division")
+    print ("N: Compute nth power of rational numbers")
     print ("Q: Quit\n")
     # Take user input for choice
-    choice = input("Please enter your choice (I to input data and Q to quit): ")
-    if choice == "I":
-      input_data()  # Call input_data function
+    choice = input("Please enter your choice (D, N, or Q to quit): ")
+    if choice == "D":
+      rat_div()  
+    elif choice == "N":
+      rat_npower()
     elif choice == "Q":
       quit()  # Call quit function and break out of loop
       break
@@ -15,28 +19,15 @@ def main_menu():
       # If user input is not one of the choices, prompt input again
       print ("Invalid input, please enter again!")
 
-def input_data():
+# Function to perform division between 2 rational numbers that the user inputted
+def rat_div():
   while True:
-    print ("a. Find the maximum")
-    print ("b. Find the minimum")
-    print ("c. Show the numbers in descending order")
-    print ("d. Show the numbers in ascending order")
+    
     print ("q. Return\n")
-    sub_choice = input ("Please enter your choice (a, b, c, d, or q to return): ")
-    if sub_choice == "a":
-      print ("The maximum number is {}.".format(maximum()))
-    elif sub_choice == "b":
-      print ("The minimum number is {}.".format(minimum()))
-    elif sub_choice == "c":
-      print ("Numbers in descending order:")
-      descending()
-    elif sub_choice == "d":
-      print("Numbers in ascending order: ")
-      ascending ()
-    elif sub_choice == "q":
-      break
-    else: 
-      print ("Invalid input, enter again!")
+    
+
+def rat_npower():
+  
 
 def quit():
   print ("Goodbye!")
