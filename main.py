cd8aeb36f30315901bc865e89edaa3782fe61507
zhlouise <116058486+zhlouise@users.noreply.github.com>
2023-11-26 07:55:31 +0000

    wip

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import no_rat
+
 # Function to display the main menu
 def main_menu():
   # Loop to display main menu, where the user is repeatly asked to choose an operation
@@ -13,7 +15,7 @@ def main_menu():
     elif choice == "N":
       rat_npower()
     elif choice == "Q":
-      quit()  # Call quit function and break out of loop
+      quit()  # Call quit function and break out of loop (End application)
       break
     else:
       # If user input is not one of the choices, prompt input again
@@ -22,16 +24,21 @@ def main_menu():
 # Function to perform division between 2 rational numbers that the user inputted
 def rat_div():
   while True:
-    
-    print ("q. Return\n")
-    
+    # Ask the user to input the 2 rational numbers for performing the division
+    a1 = int(input("Please enter the numerator of your first rational number: "))
+    b1 = int(input("Please enter the denominator of your first rational number: "))
+    # Make the rational numbers objects of class noRat
+    rat_1 = no_rat(a1, b1)
+    a2 = int(input("Please enter the numerator of your second rational number: "))
+    b2 = int(input("Please enter the denominator of your second rational number: "))
+    rat_2 = no_rat(a2, b2)
 
 def rat_npower():
-  
+  print("holder")
 
 def quit():
   print ("Goodbye!")
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   main_menu()
---
+++ no_rat.py
@@ -0,0 +1,26 @@
+class no_rat:
+  def __init__(self, a, b):
+    try:
+      rat = a/b
+      self.a = a
+      self.b = b
+    except:
+      raise ValueError("Action not allowed: denominator cannot be 0!")
+
+  # Method for calculating the greatest common divisor. Running a loop from 1 to the lowest number among a and b. Storing the value of i into n if remainder of a/i and b/i both are 0.
+  def common_div(self, a, b):
+        n = 0
+        for i in range(1, min(a, b) + 1):
+            if a % i == b % i == 0:
+                n = i
+        return n
+
+  def divide(self, other_a, other_b):
+    try:
+      new_a = self.a * other_b  #Dividing a/b by other_a/other_b
+      new_b = self.b * other_a
+      gcd = self.common_div(new_a, new_b)
+      # Dividing by gcd to get the simplest form
+      print(f"{int(new_a / gcd)} / {int(new_b / gcd)}")
+    except:
+      raise ValueError("Action not allowed: divisor = 0!")
